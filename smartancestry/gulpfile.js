00239aaa61773b7907f40ec6eaba80f26ec89288
Michael Rommel <michael.rommel@mobility-media.de>
2015-06-12 20:03:15 +0200

    refine gulp script

--- smartancestry/gulpfile.js
+++ smartancestry/gulpfile.js
@@ -1,5 +1,26 @@
+// installation: 
+// * npm install gulp gulp-changed gulp-rename gulp-image-resize gulp-imagemin --save-dev
+// * npm install --save-dev gulp-using
+// 
+// * TODO: https://github.com/rvagg/through2
+
 var gulp = require('gulp');
 var imageResize = require('gulp-image-resize');
+var parallel = require("concurrent-transform");
+var os = require("os");
+var rename = require("gulp-rename");
+var using = require('gulp-using');
+// var ancestryTree = require('ancestryTree');
+
+var fs          = require('fs');
+var path        = require('path');
+var through     = require('through2');
+var Canvas      = require('canvas');
+var PluginError = gutil.PluginError;
+
+// define paths
+var imgSrc = './data/media/persons/*',
+    imgDst = 'data/media/persons_tree/';
 
 gulp.task('default', function() {
 	console.log("###################################################");
@@ -9,24 +30,73 @@ gulp.task('default', function() {
 
 gulp.task('resize', function() {
  	console.log("resize all images");
- 	
- 	// define paths
-	var imgSrc = './originals/**/*',
-        imgDst = './optimized/';
- 	
- 	gulp.watch(imgSrc, ['thumb']);
-});
 
-gulp.task('thumb', function () {
-    gulp.src(imgSrc)
-        .pipe(imageResize({ 
+ 	gulp.src(imgSrc)
+ 		.pipe(imageResize({ 
             width : 100,
-            height : 100,
-            crop : true,
-            upscale : false,
-            imageMagick: true
+            height : 100
         }))
-        .pipe(imagemin())
-        .pipe(rename({suffix: '-thumb'}))
+        .pipe(gulpAncestryTree({ offsetX: 5 }))
         .pipe(gulp.dest(imgDst));
 });
+
+function gulpAncestryTree(opts) {
+
+    // combine with default options
+    opts = _.extend({
+        offsetX: 0,
+        offsetY: 0,
+        width: 150,
+        height: 70
+    }, opts || {});
+
+    return through.obj(function(file, enc, callback){
+        // Pass file through if:
+        // - file has no contents
+        // - file is a directory
+        if (file.isNull() || file.isDirectory()) {
+            this.push(file);
+            return callback();
+        }
+        // User's should be using a compatible glob with plugin.
+        // Example: gulp.src('images/**/*.{jpg,png}').pipe(watermark())
+        if (['.jpg', '.png'].indexOf(path.extname(file.path)) === -1) {
+            this.emit('error', new PluginError({
+                plugin: 'AncestryTree',
+                message: 'Supported formats include JPG and PNG only.'
+            }));
+            return callback();
+        }
+
+        // No support for streams
+        if (file.isStream()) {
+            this.emit('error', new PluginError({
+                plugin: 'AncestryTree',
+                message: 'Streams are not supported.'
+            }));
+            return callback();
+        }
+
+        if (file.isBuffer()) {
+            // create our virtual image
+            // and set src to file's contents
+            var img = new Canvas.Image();
+            img.src = file.contents;
+            
+            // make a new canvas with the same dimensions
+            var canvas = new Canvas(width, height);
+            var ctx = canvas.getContext('2d');
+            
+            ctx.fillRect(0, 0, width, height);
+            
+            // fill the canvas with the initial image
+            ctx.drawImage(img, offsetX, offsetY, height, height);
+            
+            // replace the file contents with our new image
+            file.contents = canvas.toBuffer();
+
+            this.push(file);
+            return callback();
+        }
+    });
+}
